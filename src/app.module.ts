4adc4c9be14a9a7e9b9fdaa95dae9918ca410ebb
VIREAK KEOSOKVIBOL <vireakvibol@gmail.com>
2022-06-29 13:19:57 +0700

    disable graphql playground and debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,19 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AdminModule } from './admin/admin.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'graphql/schema.gql'),
-      debug: true,
-      playground: true,
+      debug: false,
+      playground: false,
       sortSchema: true,
     }),
     AdminModule,
   ],
+  providers: [PrismaService],
 })
 export class AppModule {}
